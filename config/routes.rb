8aa286691ef95b9dcf7f6a6b723c12194a532207
Robin Doirin <robindoirin@gmail.com>
2018-07-24 15:05:32 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :preferences, only: [:index]
   end
+
   resources :artists do
     resources :songs, only: [:index, :show]
   end
