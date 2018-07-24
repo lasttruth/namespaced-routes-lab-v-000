30679f007840b35f47ed51b94a582f844b2e57e1
Robin Doirin <robindoirin@gmail.com>
2018-07-24 15:11:56 +0000

    automatically backed up by learn

--- db/migrate/20180724151138_create_preference.rb
+++ db/migrate/20180724151138_create_preference.rb
@@ -1,6 +1,11 @@
 class CreatePreference < ActiveRecord::Migration
   def change
     create_table :preferences do |t|
+      create_table :preferences do |t|
+      t.string :artist_sort_order
+      t.string :song_sort_order
+      t.boolean :allow_create_artists
+      t.boolean :allow_create_songs
     end
   end
 end
